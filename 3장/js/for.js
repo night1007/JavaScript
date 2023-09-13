d8397b6db92eec7d7457864558c07d3de368ffee
night1007 <144084205+night1007@users.noreply.github.com>
2023-09-13 15:08:42 +0900

    Add files via upload

---
+++ 3장/js/for.js
@@ -0,0 +1,5 @@
+const students = ["박준우", "김상헌", "권민석", "장준영"];
+
+for(let i = 0; i < students.length; i++) {
+    document.write(`${students[i]}<br>`);
+}
---
+++ 3장/js/forEach.js
@@ -0,0 +1,5 @@
+const students = ["박준우", "김상헌", "권민석", "장준영"];
+
+students.forEach(function(student) {
+    document.write(`${student} `) 
+});
